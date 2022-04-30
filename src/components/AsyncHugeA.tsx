42cc9e253240f3bd18081cb5602c5b36e5800f7a
后厂村小王 <48429995+EricWong1994@users.noreply.github.com>
2022-04-30 22:55:58 +0800

    delete layout

---
+++ src/components/AsyncHugeA.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HugeA from './HugeA';
+
+// 像使用普通组件一样即可
+// dynamic 为你做:
+// 1. 异步加载该模块的 bundle
+// 2. 加载期间 显示 loading（可定制）
+// 3. 异步组件加载完毕后，显示异步组件
+
+export default () => {
+  return <HugeA />;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import styles from './index.less';
+import React from 'react';
+// import AsyncHugeA from '../components/AsyncHugeA'
 
 export default function IndexPage() {
   return (
     <div>
+      {/* <AsyncHugeA></AsyncHugeA> */}
       <h1 className={styles.title}>Page index</h1>
     </div>
   );
